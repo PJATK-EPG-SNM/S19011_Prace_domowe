4366258a15d57e5a42dce1cff2cb673ee845db81
s19011-pja <61684739+s19011-pja@users.noreply.github.com>
2020-03-13 22:41:28 +0100

    First_Game_v.0.01
    Gracz (+)
    Ruchoma platforma (+)
    Znikająca platforma (-)
    Moja platforma (-)
    Własne assety (+)

---
+++ Praca_domowa_01/First_Game/Assets/Scripts/Vanish.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vanish : MonoBehaviour
+{
+
+	public float timer = 2f;
+	public GameObject Player;
+	
+//	private float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+		
+//        if (timer > 0)
+//			{
+//				timer -= Time.deltaTime;
+//				if (timer <= 0)
+//				{
+//				 Destruction();
+//				}
+//			}
+    }
+	
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "Player"){
+//			if (timer > 0)
+//			{
+//				timer -= Time.deltaTime;
+//				if (timer <= 0)
+//				{
+				 Destruction();
+//				}
+//			}
+			
+		}
+	}
+	
+//	private void OnTriggerEnter(Collider other)
+//	{
+//		if (other.gameObject == Player)
+//		{
+////			if (timer > 0)
+////			{
+////				timer -= Time.deltaTime;
+////				if (timer <= 0)
+////				{
+//				 Destruction();
+////				}
+////			}
+//		}
+//	}
+	
+	void Destruction()
+	{
+		Destroy(this.gameObject);
+	}
+}
