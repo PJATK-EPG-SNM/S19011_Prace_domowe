e053c7f198544f21b13e197e43b0fcb7b3fa3bc0
s19011-pja <61684739+s19011-pja@users.noreply.github.com>
2020-03-24 18:37:24 +0100

    Worlds_v.0.01
    "WORLDS", ćwiczenie Tile Map Editor
    - 2 zestawy sprajtów (16x16)
    - levele zapisane w dwóch scenach (DinoGaj, ElfiGaj)
    - 2 Tile Palette (dinogaj, elfigaj_proba)
    - 2 playery (dinogaj_player2, elfigaj_player1)

---
+++ Praca_domowa_02/Worlds/Assets/Scripts/Vanish.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vanish : MonoBehaviour
+{
+
+	public GameObject Player;
+	public float timer = 2f;
+   
+	private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject == Player)
+			{
+			Destroy(this.gameObject, timer);
+//				
+			
+			}
+		}
+		
+	private void OnTriggerExit2D(Collider2D other)
+		{
+			if (other.gameObject == Player)
+			{
+				
+			}
+			
+		}
+}
