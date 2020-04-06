0b78e9232007eb7ff337a0c00065c045e7e593cc
s19011-pja <61684739+s19011-pja@users.noreply.github.com>
2020-04-06 13:32:10 +0200

    Worlds_0.04
    Ćwiczenie w Animatorze
    - OnTriggerEnter2D

---
+++ Praca_domowa_02/Worlds/Assets/Scripts/PlatformTestScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTestScript : MonoBehaviour
+{
+
+      public GameObject Player;
+      Animator  animator;
+      bool platformUp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformUp = false;
+        animator = GetComponent<Animator>();
+    }
+
+
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+    
+        if (col.gameObject == Player)
+        {
+            platformUp = true;
+            Debug.Log("Platforma na górze");
+            PlatformTestControl("Up");
+		}
+	}
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (platformUp)
+        {
+             platformUp = false;
+             Debug.Log("Platforma na dole");
+             PlatformTestControl("Down");
+		}
+        
+
+	}
+
+    void PlatformTestControl(string direction)
+    {
+        animator.SetTrigger(direction);
+	}
+
+}
